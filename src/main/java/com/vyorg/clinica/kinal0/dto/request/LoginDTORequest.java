4e992e256a6c8fcc33c73d9dd7de976c7dc86eb7
Victor502-67 <vyoc-2026210@kinal.edu.gt>
2026-09-17 21:07:23 -0600

    Creacion del login funcional

---
+++ src/main/java/com/vyorg/clinica/kinal0/dto/request/LoginDTORequest.java
@@ -0,0 +1,27 @@
+package main.java.com.vyorg.clinica.kinal0.dto.request;
+
+public class LoginDTORequest {
+    private String nombreUsuario;
+    private String password;
+
+    public LoginDTORequest(String nombreUsuario, String password) {
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/vyorg/clinica/kinal0/repository/AccesoRepository.java
@@ -0,0 +1,37 @@
+package main.java.com.vyorg.clinica.kinal0.repository;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import main.java.com.vyorg.clinica.kinal0.config.DatabaseConnection;
+import main.java.com.vyorg.clinica.kinal0.dto.request.LoginDTORequest;
+import main.java.com.vyorg.clinica.kinal0.dto.response.LoginDTOResponse;
+
+public class AccesoRepository {
+
+    public LoginDTOResponse findUserByNombreUsuario(LoginDTORequest loginDTORequest) {
+        String sql = "select u.id_usuario, u.nombre_completo, u.nombre_usuario, " +
+                     "u.password_hash, u.activo, r.nombre_rol " +
+                     "from usuarios as u " +
+                     "inner join roles as r on u.id_rol = r.id_rol " +
+                     "where u.nombre_usuario = ? ";
+
+        try (PreparedStatement pstm = DatabaseConnection.getDatabaseConnection().prepareStatement(sql)) {
+            pstm.setString(1, loginDTORequest.getNombreUsuario());
+            ResultSet rs = pstm.executeQuery();
+            if (rs.next()) {
+                return new LoginDTOResponse(
+                        rs.getInt("id_usuario"),
+                        rs.getString("nombre_completo"),
+                        rs.getString("nombre_usuario"),
+                        rs.getString("password_hash"),
+                        rs.getString("nombre_rol"),
+                        rs.getBoolean("activo")
+                );
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al buscar el usuario: " + e.getMessage());
+        }
+        return null;
+    }
+}
